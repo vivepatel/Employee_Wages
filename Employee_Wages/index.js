88176f35c52d9ab40cc8027c5b23d635f8b5100d
Vivekpatel <vp20032020@gmail.com>
2025-03-04 16:32:25 +0530

    [Vivek] Added UC3

--- Employee_Wages/index.js
+++ Employee_Wages/index.js
@@ -1,25 +1,28 @@
-
 const WAGE_PER_HOUR = 20;
 const FULL_TIME_HOURS = 8;
 const PART_TIME_HOURS = 4;
 
-let workType = Math.floor(Math.random() * 3);
-let workHours = 0;
-
-switch (workType) {
-    case 1:
-        workHours = PART_TIME_HOURS;
-        console.log("Employee worked Part Time");
-        break;
-    case 2:
-        workHours = FULL_TIME_HOURS;
-        console.log("Employee worked Full Time");
-        break;
-    default:
-        workHours = 0;
-        console.log("Employee did not work");
-        break;
+function getWorkHours(workType) {
+    let workHours;
+    switch (workType) {
+        case 1:
+            workHours = PART_TIME_HOURS;
+            console.log("Employee worked Part Time");
+            break;
+        case 2:
+            workHours = FULL_TIME_HOURS;
+            console.log("Employee worked Full Time");
+            break;
+        default:
+            workHours = 0;
+            console.log("Employee did not work");
+            break;
+    }
+    return workHours;
 }
 
+let workType = Math.floor(Math.random() * 3); // Generates 0, 1, or 2
+let workHours = getWorkHours(workType);
 let dailyWage = workHours * WAGE_PER_HOUR;
+
 console.log("Daily Wage: $" + dailyWage);
