f52022999cf0085244ef399f4076981b139a2d6b
Vivekpatel <vp20032020@gmail.com>
2025-03-04 16:21:02 +0530

    [Vivek] Added UC2

--- Employee_Wages/index.js
+++ Employee_Wages/index.js
@@ -1,10 +1,25 @@
-const IS_ABSENT =0;
-let empCheck = Math.floor(Math.random() * 10) %2;
-if(empCheck == IS_ABSENT)
-{
-    console.log("Employee is Absent");
-    return;
+
+const WAGE_PER_HOUR = 20;
+const FULL_TIME_HOURS = 8;
+const PART_TIME_HOURS = 4;
+
+let workType = Math.floor(Math.random() * 3);
+let workHours = 0;
+
+switch (workType) {
+    case 1:
+        workHours = PART_TIME_HOURS;
+        console.log("Employee worked Part Time");
+        break;
+    case 2:
+        workHours = FULL_TIME_HOURS;
+        console.log("Employee worked Full Time");
+        break;
+    default:
+        workHours = 0;
+        console.log("Employee did not work");
+        break;
 }
-else{
-    console.log("Employeee is Present");
-}
+
+let dailyWage = workHours * WAGE_PER_HOUR;
+console.log("Daily Wage: $" + dailyWage);
