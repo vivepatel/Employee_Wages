fb912e5b7f3f701938ade9ba483c254aeb805371
Vivekpatel <vp20032020@gmail.com>
2025-03-06 00:11:17 +0530

    [Vivek] Added UC13

--- Employee_Wages/EmployeePayroll.js
+++ Employee_Wages/EmployeePayroll.js
@@ -1,15 +1,20 @@
+
 class EmployeePayroll {
-    constructor(id, name, salary) {
+    constructor(id, name, salary, gender, startDate) {
         this.id = id;
         this.name = name;
         this.salary = salary;
+        this.gender = gender;
+        this.startDate = new Date(startDate);
     }
 
-    getDetails = () => `ID: ${this.id}, Name: ${this.name}, Salary: $${this.salary}`;
+   
+    getDetails = () => 
+        `ID: ${this.id}, Name: ${this.name}, Salary: $${this.salary}, Gender: ${this.gender}, Start Date: ${this.startDate.toDateString()}`;
 }
 
-const emp1 = new EmployeePayroll(101, "Muskan Gupta", 50000);
-const emp2 = new EmployeePayroll(102, "Nancy Mehra", 45000);
+const emp1 = new EmployeePayroll(101, "Muskan Gupta", 50000, "Female", "2023-05-10");
+const emp2 = new EmployeePayroll(102, "Nancy Mehra", 47000, "Female", "2022-11-15");
 
 console.log(emp1.getDetails());
 console.log(emp2.getDetails());
