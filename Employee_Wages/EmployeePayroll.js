c81f1b09c399a41ebe54e098c6d9f3868cda8434
Vivekpatel <vp20032020@gmail.com>
2025-03-06 00:15:13 +0530

    [Vivek] Added UC14

--- Employee_Wages/EmployeePayroll.js
+++ Employee_Wages/EmployeePayroll.js
@@ -2,10 +2,22 @@
 class EmployeePayroll {
     constructor(id, name, salary, gender, startDate) {
         this.id = id;
-        this.name = name;
         this.salary = salary;
         this.gender = gender;
         this.startDate = new Date(startDate);
+        this.setName(name);
+    }
+
+    setName(name) {
+        try {
+            const nameRegex = /^[A-Z][a-zA-Z]{2,}$/;
+            if (!nameRegex.test(name)) {
+                throw new Error("Invalid Name! Name must start with a capital letter and have at least 3 characters.");
+            }
+            this.name = name;
+        } catch (error) {
+            console.error(error.message);
+        }
     }
 
    
@@ -13,8 +25,11 @@ class EmployeePayroll {
         `ID: ${this.id}, Name: ${this.name}, Salary: $${this.salary}, Gender: ${this.gender}, Start Date: ${this.startDate.toDateString()}`;
 }
 
-const emp1 = new EmployeePayroll(101, "Muskan Gupta", 50000, "Female", "2023-05-10");
-const emp2 = new EmployeePayroll(102, "Nancy Mehra", 47000, "Female", "2022-11-15");
+
+const emp1 = new EmployeePayroll(101, "Muskan", 50000, "Female", "2023-05-10");  
+const emp2 = new EmployeePayroll(102, "Nancy", 47000, "Female", "2022-11-15"); 
+const emp3 = new EmployeePayroll(103, "Pra", 46000, "Male", "2024-01-20"); 
 
 console.log(emp1.getDetails());
-console.log(emp2.getDetails());
+console.log(emp2.getDetails()); 
+console.log(emp3.getDetails()); 
