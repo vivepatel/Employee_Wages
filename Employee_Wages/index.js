793f6c772716e3dcc5d3c7c973bc85a784bf5e40
Vivekpatel <vp20032020@gmail.com>
2025-03-05 23:58:37 +0530

    [Vivek] Added UC9

--- Employee_Wages/index.js
+++ Employee_Wages/index.js
@@ -23,6 +23,8 @@ let totalDays = 0;
 let dailyWages = []; 
 let dailyRecords = []; 
 let dailyWageMap = new Map();
+let dailyHourMap = new Map();
+
 
 while (totalDays < MAX_WORKING_DAYS && totalHours < MAX_WORKING_HOURS) {
     let workType = Math.floor(Math.random() * 3); 
@@ -37,13 +39,20 @@ while (totalDays < MAX_WORKING_DAYS && totalHours < MAX_WORKING_HOURS) {
     totalDays++;
 
     dailyWageMap.set(totalDays, dailyWage);
+    dailyHourMap.set(totalDays, workHours);
 }
 
 
 let totalWage = Array.from(dailyWageMap.values()).reduce((sum, wage) => sum + wage, 0);
-console.log(`Total Monthly Wage: $${totalWage}`);
+const totalWorkedHours = Array.from(dailyHourMap.values()).reduce((sum, hours) => sum + hours, 0);
+console.log(`Total Wage: $${totalWage}, Total Hours Worked: ${totalWorkedHours} hrs`);
 
 console.log("Day-wise Wages:", Array.from(dailyWageMap.entries()).map(([day, wage]) => `Day ${day}: $${wage}`));
+
+
+const fullWorkDays = Array.from(dailyHourMap.entries()).filter(([day, hours]) => hours === FULL_TIME_HOURS).map(([day]) => `Day ${day}`);
+const partWorkDays = Array.from(dailyHourMap.entries()).filter(([day, hours]) => hours === PART_TIME_HOURS).map(([day]) => `Day ${day}`);
+const noWorkDays = Array.from(dailyHourMap.entries()).filter(([day, hours]) => hours === 0).map(([day]) => `Day ${day}`);
 
 
 let fullTimeDays = dailyRecords.filter(record => record.wage === FULL_TIME_WAGE);
